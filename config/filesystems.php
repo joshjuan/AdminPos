ced13e89b31077f23de954481c90c1c63dc0f7de
joshua.mwafulango <joshua.mwafulango@pccb.go.tz>
2025-01-10 21:36:32 +0300

    changing path of storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,7 +50,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+           // 'root' => storage_path('app/public'),
+            'root' => storage_path('storage'), // Set the root path to public/storage
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
